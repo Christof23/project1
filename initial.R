55338a0419ccf9bd14d5f4494a5294f6a91a78f6
Christof23 <30445221+Christof23@users.noreply.github.com>
2019-05-23 21:12:36 +0100

    remove

--- initial.R
+++ initial.R
@@ -1,9 +1,4 @@
 library(tidyverse)
 library(data.table)
 
-x=fread('political_parties.csv') %>% as_tibble()
-
-# galtan - Libertarian or postmaterialist
-# redistribution - economic left/right spectrum
-
-
+x=fread('') %>% as_tibble() 
